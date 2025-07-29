8f5a0a8d8148bf0d3e3bc1ef992172b8a8cfa914
Utsav Gamee <utsavgami88@gmail.com>
2025-07-29 21:07:37 +0530

    update configuration

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
+
+
 export default defineConfig({
+  base: '/my-portfolio/',
   plugins: [react()],
 })
