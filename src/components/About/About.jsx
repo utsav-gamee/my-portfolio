37a2bc57733cd84909dbc8afc346d523f2baeb29
Utsav Gamee <utsavgami88@gmail.com>
2025-07-29 20:57:09 +0530

    project added

---
+++ src/components/About/About.jsx
@@ -0,0 +1,39 @@
+import pfp from "./Profile.png";
+import { stackList } from "../../data/ProjectData";
+import {
+  Image,
+  Technologies,
+  Tech,
+  TechImg,
+  TechName,
+  ContactWrapper,
+} from "./AboutElements";
+function About() {
+  return (
+    <ContactWrapper id="about">
+      <div className="Container">
+        <div className="SectionTitle">About Me</div>
+        <div className="BigCard">
+          <Image src={pfp} alt="man-svgrepo" />
+          <div className="AboutBio">
+            Hello! My name is <strong>Utsav Gamee</strong> and I enjoy creating
+            things that live on the internet.
+          </div>
+          <div className="AboutBio tagline2">
+            I have become confident using the following technologies.
+          </div>
+          <Technologies>
+            {stackList.map((stack, index) => (
+              <Tech key={index} className="tech">
+                <TechImg src={stack.img} alt={stack.name} />
+                <TechName>{stack.name}</TechName>
+              </Tech>
+            ))}
+          </Technologies>
+        </div>
+      </div>
+    </ContactWrapper>
+  );
+}
+
+export default About;
